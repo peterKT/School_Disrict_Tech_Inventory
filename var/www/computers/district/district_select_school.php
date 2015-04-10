612cbf3de2b37bd4ea8b5318ead3694a61870d78
Peter Titus <pkt650@gmail.com>
2015-04-10 12:04:22 -0400

    Create district_select_school.php

---
+++ var/www/computers/district/district_select_school.php
@@ -0,0 +1,64 @@
+
+<?php
+$page_title = 'Select a School';
+
+
+include ('../../includes/header_district_computers.html');
+
+?>
+<h2>Select a building to view inventory</h2>
+
+<form action="district_view_computers.php" method="post">
+<fieldset><legend>Select a School</legend>
+
+
+<?php
+
+require_once ('../../../mysql_connect_computers.php');
+
+
+$query = "select school_id,school from schools where school_id != 23 order by school" ;
+
+$result = @mysql_query($query);
+
+
+if ($result) {
+ 
+  echo '<select name="schools">';
+  while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+  	echo '<option value="' . $row['school_id'] . '">' . ' ' . $row['school'] .  '</option>\\n';
+}
+ echo '</select>'; 
+	
+mysql_free_result ($result);
+
+} else {
+  echo '<p class="error">The schools could not be retrieved. 
+We apologize for any inconvenience.</p>';
+
+  echo '<p>' . mysql_error() . '<br /><br />Query: ' . $query . '</p>';
+
+
+mysql_close();  
+exit();
+ }
+
+
+?>
+
+<!-- 
+<p>Sort by: <input type="radio" name="sort" value="model" />Model 
+<input type="radio" name="sort" value="rooms.room_id" />Room
+ -->
+</fieldset>
+<div align="center"><input type="submit" name="submit" value="Submit 
+School Selection" /></div>
+</form>
+
+
+
+
+</body>
+</html>
+
+
